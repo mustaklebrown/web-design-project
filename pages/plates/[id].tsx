e5f4148a3bcdb6da3722dcf3ba2fa9cd7891e078
mustaklebrown <embdoullah1998@gmail.com>
2023-01-21 19:08:37 +0300

    portfolio detail again

--- pages/plates/[id].tsx
+++ pages/plates/[id].tsx
@@ -8,7 +8,7 @@ interface Props {
 
 }
 
-const PortfolioDetails: Props = () => {
+const PortfolioDetails: Props = (props) => {
     const router = useRouter()
     const { id } = router.query
     const rid = Number(id)
@@ -19,8 +19,8 @@ const PortfolioDetails: Props = () => {
         <div className='max-w-7xl w-full min-h-screen mx-auto p-4 xl:p-0 flex flex-col items-center justify-center   '>
             <div className='md:flex items-start md:gap-10 space-y-10 md:space-y-0'>
 
-                <div className='md:w-[46%] overflow-hidden w-full max-h-[700px]' >
-                    <Image src={`${plates?.image}`} width={600} height={700} alt='rest-image' className='w-full h-full rounded-md object-cover object-center' />
+                <div className='md:w-[46%] overflow-hidden w-full max-h-[600px]' >
+                    <Image src={`${plates?.image}`} width={600} height={600} alt='rest-image' className='w-full h-full rounded-md object-cover object-center' />
                 </div>
                 <div className='md:w-[46%] text-center md:text-start'>
                     <h1 className='text-2xl uppercase text-yellow-500 font-extrabold'>
